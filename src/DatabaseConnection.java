5b3e31c14f06c939d04b730498dad8fc56d60b3f
Shyamanjee <2022s19419@stu.cmb.ac.lk>
2025-09-07 09:55:16 +0530

    structure done

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -3,11 +3,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/studentdb?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+
+    private static final String USER = "studentuser";
+    private static final String PASSWORD = "student123";
+    // your MySQL password
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
